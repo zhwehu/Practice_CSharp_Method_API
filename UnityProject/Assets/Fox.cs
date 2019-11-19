c981368c5000dd103a47fb9c382eedad386ab2a7
KID421 <suchild7@gmail.com>
2019-11-19 16:46:01 +0800

    完成版本

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f, 50f)]
+    public float speed = 10f;
+
+    public Rigidbody2D rig;
+    public SpriteRenderer sr;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rig.AddForce(new Vector2(h * speed, 0));
+        sr.flipX = h < 0;
+    }
+}
